3da2380d7957f53a175287a0659e6e87d5f81635
esron <esron.dtamar@gmail.com>
2017-07-21 21:43:28 -0300

    Serviço jogador 2 testar nó terminal, State métodos auxiliáres

--- src/app/player-2.service.ts
+++ src/app/player-2.service.ts
@@ -8,6 +8,45 @@ export class Player2Service {
   constructor() { }
 
   play(board:string[][]) {
-    
+    // let currentState: State = new State(board, 0, null);
+    let currentState: State = new State([board[0], board[1], board[2]], 0, null, [0, 0]);
+    console.log("Estado inicial\n" + currentState.toString());
+
+    currentState.getChilds().forEach(child => {
+      console.log(child.toString());
+    });
+
+    return [1, 1];
+  }
+
+  isTerminalNode(board) {
+    for(let i = 0; i < 3; i++) {
+      // Checar linhas
+      if(board[i][0] == board[i][1] && board[i][0] == board[i][2]
+        && (board[i][0] == 'X' || board[i][0] == 'O')) {
+        return true;
+      }
+
+      // Checar colunas
+      if(board[0][i] == board[1][i] && board[0][i] == board[2][i]
+        && (board[0][i] == 'X' || board[0][i] == 'O')) {
+        return true;
+      }
+    }
+
+    // Checar diagonais
+    if(((board[0][0] == board[1][1] && board[0][0] == board[2][2])
+      || (board[0][2] == board[1][1] && board[0][2] == board[2][0]))
+      && (board[1][1] == 'X' || board[1][1] == 'O')) {
+        return true;
+    }
+
+    // Checar empate
+    for(let i = 0; i < 3; i++)
+      for(let j = 0; j < 3; j++){
+        if(board[i][j] == ' ')
+          return false;
+      }
+    return true;
   }
 }
--- src/app/state.ts
+++ src/app/state.ts
@@ -1,14 +1,20 @@
 export class State {
-    board: string[][];
+    board: string[][] = [[],[],[]];
     hn: number;
     gn: number;
     fn: number;
     parent: State
+    played:number[] = [0, 0];
 
-    constructor(board: string[][], depth: number, parent: State) {
-        for (let i = 0; i < 3; i++)
-            for (let j = 0; j < 3; j++)
+    constructor(board: string[][], depth: number, parent: State, played:number[]) {
+        for (let i = 0; i < 3; i++) {
+            this.board[i] = [];
+            for (let j = 0; j < 3; j++) {
                 this.board[i][j] = board[i][j];
+            }
+        }
+        this.played[0] = played[0];
+        this.played[1] = played[1];
         this.gn = depth;
         this.parent = parent;
     }
@@ -21,12 +27,44 @@ export class State {
         return true;
     }
 
+    copy(state: State) {
+        return new State(state.board, state.gn, state.parent, state.played);
+    }
+
+    setParent(state: State) {
+        this.parent = state;
+        return this;
+    }
+
+    setPlayed(i:number, j:number) {
+        this.played[0] = i;
+        this.played[1] = j;
+    }
+
     toString() {
         let string = "board:\n";
         
         for(let i = 0; i < 3; i++)
             string += "["+this.board[i][0]+","+this.board[i][1]+","+this.board[i][2]+"]\n";
-
         string += "h(n) = " + this.hn;
+
+        return string;
+    }
+
+    getChilds() {
+        let childs: State[] = [];
+        
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if(this.board[i][j] == ' ') {
+                    let child = this.copy(this).setParent(this);
+                    child.board[i][j] = 'O';
+                    child.setPlayed(i, j);
+                    childs.push(child);
+                }
+            }
+        }
+
+        return childs;
     }
 }
